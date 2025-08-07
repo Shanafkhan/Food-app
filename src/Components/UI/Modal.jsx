a4bcf0015d768d70456d6dec0fa658444f7bb22d
ShanafKhan <shanafkhan4@gmail.com>
2025-08-07 17:22:08 +0530

    Handled the form data and send it to the backend

--- src/Components/UI/Modal.jsx
+++ src/Components/UI/Modal.jsx
@@ -1,6 +1,6 @@
 import { createPortal } from "react-dom";
 import { useRef, useEffect } from "react";
-export default function Modal({ children, open , className = '' }) {
+export default function Modal({ children, open , className = '' , onClose}) {
     const dialog = useRef();
     useEffect(()=>{
         const modal = dialog.current;
@@ -12,7 +12,7 @@ export default function Modal({ children, open , className = '' }) {
     },[open])
 
   return createPortal(
-    <dialog  ref={dialog} className={`modal ${className}`}>{children}</dialog>,
+    <dialog  ref={dialog} className={`modal ${className}`} onClose={onClose}>{children}</dialog>,
     document.getElementById("modal")
   );
 }
